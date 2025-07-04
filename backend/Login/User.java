94f6479452e1809aa476fed174a8637fcf5f5510
MihneaCucu <cucumihnea0@gmail.com>
2025-07-04 12:53:30 +0300

    Created User class

---
+++ backend/Login/User.java
@@ -0,0 +1,33 @@
+package Login;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    @Override
+    public String toString() {
+        return "Login.User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
