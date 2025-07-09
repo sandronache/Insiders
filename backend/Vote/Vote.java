e2af71225bb24135f4da690e265e3ef90fdefc55
Valinxv <andrei.nedelcu2002@stud.acs.upb.ro>
2025-07-09 14:13:42 +0300

    Added toggleVote method for both Up/Downvotes avoiding duplicates.

--- backend/Vote/Vote.java
+++ backend/Vote/Vote.java
@@ -14,37 +14,36 @@ public class Vote {
         this.isEmoji = false;
     }
 
-    public void addUpVote(String username) {
-        if(upvotes.contains(username))
-            upvotes.remove(username);
-        else {
-                /* in cazul in care user-ul nu a dat nici un vot, atunci downvotes.remove nu va sterge nimic ca nu exista.
-                asta pentru a evita cod redundant cu 3 if-uri */
-            downvotes.remove(username);
-            upvotes.add(username);
+    private void toggleVote(Set<String> first, Set<String> second, String username) {
+        if (first.contains(username)) {
+            first.remove(username);
+        } else {
+            second.remove(username);
+            first.add(username);
         }
         checkEmoji();
     }
 
-    public void addDownVote(String username){
-        if(downvotes.contains(username))
-            downvotes.remove(username);
-        else {
-            // aceeasi logica dar mirrored.
-            upvotes.remove(username);
-            downvotes.add(username);
-        }
-        checkEmoji();
+    public void addUpVote(String username){
+        toggleVote(upvotes, downvotes, username);
     }
+
+    public void addDownVote(String username) {
+        toggleVote(downvotes, upvotes, username);
+    }
+
     private void checkEmoji() {
         isEmoji = upvotes.size() >= 10;
     }
+
     public int getUpvoteCount() {
         return upvotes.size();
     }
+
     public int getDownvoteCount(){
         return downvotes.size();
     }
+
     public boolean isEmoji(){
         return isEmoji;
     }
