9fe776a510e4637d596bc94cd48eb1b750e6f679
sandronache <120741877+sandronache@users.noreply.github.com>
2025-07-17 15:22:33 +0300

    more docker implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,18 @@ import main.java.logger.ILogger;
 import main.java.logger.LoggerFacade;
 import main.java.service.*;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
+        final String loggerLocation = Objects.isNull(System.getenv("IS_DEV"))? "/mnt/resources/application.log" : "resources/application.log";
 
         //ILogger consoleLogger = new ConsoleLogger();
-        ILogger fileLogger = new FileLogger("application.log");
+        ILogger fileLogger = new FileLogger(loggerLocation);
 
         //LoggerFacade.addLogger(consoleLogger);
         LoggerFacade.addLogger(fileLogger);
-
+        LoggerFacade.info("new version");
         LoggerFacade.info("Application starting up");
 
         FilesService.getInstance();
