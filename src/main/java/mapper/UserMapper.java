b5570b671d836151dc28fdd828a3b915cd8780ab
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2025-08-06 15:59:55 +0300

    created UserController (+ DTOs & methods in UserService) for tests in PostMan

---
+++ src/main/java/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package main.java.mapper;
+
+import main.java.dto.user.UserResponseDto;
+import main.java.entity.User;
+
+import java.time.LocalDateTime;
+
+public class UserMapper {
+    public static UserResponseDto toDto(User user){
+        return new UserResponseDto(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getCreatedAt()
+        );
+    }
+}
